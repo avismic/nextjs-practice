655192576cb50ba20b6ae90f91ab3d73741717fe
abhishek <abhishekanandsm0311@gmail.com>
2025-01-01 00:00:03 +0530

    lgtm

--- app/components/ProductCart.tsx
+++ app/components/ProductCart.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import AddToCart from "./AddToCart";
-import styles from "./ProductCart.module.css";
+
 
 const ProductCart = () => {
   return (
-    <div className={styles.card}>
+    <div >
       <AddToCart></AddToCart>
     </div>
   );
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 
 const UsersPage = async () => {
@@ -12,12 +13,23 @@ const UsersPage = async () => {
   return (
     <>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <tr key={user.id}>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          </tr>
         ))}
-      </ul>
+        </tbody>
+        
+      </table>
     </>
   );
 };
